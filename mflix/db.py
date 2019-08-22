2b01ec35d3b8609aa7a86f7d7dca1bf517db9eed
zubrzubr <maxzub@gmail.com>
2019-08-22 23:06:22 +0300

    Implement first lab with countries projection

--- mflix/db.py
+++ mflix/db.py
@@ -62,26 +62,17 @@ def get_movies_by_country(countries):
     Finds and returns movies by country.
     Returns a list of dictionaries, each dictionary contains a title and an _id.
     """
-    try:
 
-        """
-        Ticket: Projection
+    """
+    Ticket: Projection
 
-        Write a query that matches movies with the countries in the "countries"
-        list, but only returns the title and _id of each movie.
+    Write a query that matches movies with the countries in the "countries"
+    list, but only returns the title and _id of each movie.
 
-        Remember that in MongoDB, the $in operator can be used with a list to
-        match one or more values of a specific field.
-        """
-
-        # TODO: Projection
-        # Find movies matching the "countries" list, but only return the title
-        # and _id. Do not include a limit in your own implementation, it is
-        # included here to avoid sending 46000 documents down the wire.
-        return list(db.movies.find().limit(1))
-
-    except Exception as e:
-        return e
+    Remember that in MongoDB, the $in operator can be used with a list to
+    match one or more values of a specific field.
+    """
+    return list(db.movies.find({'countries': {'$in': countries}}, {'title': 1, '_id': 1}))
 
 
 def get_movies_faceted(filters, page, movies_per_page):
