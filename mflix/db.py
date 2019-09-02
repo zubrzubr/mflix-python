cbeec4623b90f851810eb11f73d17acc9b0ccb9b
zubrzubr <maxzub@gmail.com>
2019-09-03 00:17:42 +0300

    Implement report

--- mflix/db.py
+++ mflix/db.py
@@ -509,12 +509,9 @@ def most_active_commenters():
 
     No field projection necessary.
     """
-    # TODO: User Report
-    # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [{'$sortByCount':  '$email'}, {"$limit": 20}]
 
-    rc = db.comments.read_concern # you may want to change this read concern!
-    comments = db.comments.with_options(read_concern=rc)
+    comments = db.comments.with_options(read_concern=ReadConcern('majority'))
     result = comments.aggregate(pipeline)
     return list(result)
 
