793e767c87aa7d63ce56a765632b89f8891b3f96
zubrzubr <maxzub@gmail.com>
2019-09-09 09:55:13 +0300

    Handle invalid id

--- mflix/db.py
+++ mflix/db.py
@@ -255,7 +255,7 @@ def get_movie(id):
 
     # TODO: Error Handling
     # If an invalid ID is passed to `get_movie`, it should return None.
-    except (StopIteration) as _:
+    except InvalidId as ex:
 
         """
         Ticket: Error Handling
@@ -264,7 +264,7 @@ def get_movie(id):
         StopIteration exception is handled. Both exceptions should result in
         `get_movie` returning None.
         """
-
+        print('Invalid id: %s', ex)
         return None
 
     except Exception as e:
